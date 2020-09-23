94bfc17beddca31d489784ce99714e70bf350525
Michael Lee <lisenpeng007@gmail.com>
2020-09-23 20:33:23 +0800

    Updates

---
+++ precache-manifest.ed128a22910458e3afed379ae92147ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bdc4e2a94b9a31f2d0b9af34e1cde6",
+    "url": "/dns-mdm-generator/index.html"
+  },
+  {
+    "revision": "606f69f3ffa896f0e737",
+    "url": "/dns-mdm-generator/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fdd343279e281cc6681d",
+    "url": "/dns-mdm-generator/static/js/2.2ff57df9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dns-mdm-generator/static/js/2.2ff57df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606f69f3ffa896f0e737",
+    "url": "/dns-mdm-generator/static/js/main.bcbbc589.chunk.js"
+  },
+  {
+    "revision": "d9034b49160c3eab3a5b",
+    "url": "/dns-mdm-generator/static/js/runtime-main.9915419c.js"
+  }
+]);
